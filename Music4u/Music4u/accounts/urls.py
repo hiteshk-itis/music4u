04558b320d65ae4db57c66cf68240b8c283598b3
charchit319 <60282550+charchit319@users.noreply.github.com>
2022-11-22 21:41:48 +0545

    Add files via upload

--- Music4u/Music4u/accounts/urls.py
+++ Music4u/Music4u/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("register/",views.register),
     path("login/",views.login),
+    path("logout/",views.logout),
 
     # path("login/",views.login),
 ]
